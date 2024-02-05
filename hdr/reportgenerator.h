fad08f429ee29007f648533af99979575b43ff00
mahnoorzia101 <148098944+mahnoorzia101@users.noreply.github.com>
2024-02-05 09:49:08 +0500

    Add files via upload

---
+++ hdr/reportgenerator.h
@@ -0,0 +1,7 @@
+#ifndef REPORTGENERATOR_H
+#define REPORTGENERATOR_H
+
+void processWeatherData(FILE *inputFile, FILE *outputFile);
+void GeneratingReport(const char *inputFileName, const char *outputFileName);
+
+#endif  // REPORTGENERATOR_H
